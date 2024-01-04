f838b4530d3ac98bf758480a85b5d17b350dfb5a
Juandavidc08 <>
2024-01-04 15:34:47 +0000

    Show question on the browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
-// questions of the quiz
-const questions = [
-    {
-    question: 'How old is the Earth?'
-    answers: [
-        { text: '4.54 billion years old', correct : true },
-        { text: '10 billion years old', correct : false },
-        { text: '1.71 billion years old', correct : false },
-        {text: '3.96 billion years old', correct: false}
-    ]
-    }
-]
+
 
 // Shuffle question so everytime the quiz have diferent order
-const shuffleQuestions, currentQuestionIndex 
+let shuffleQuestions, currentQuestionIndex 
 
 //Start and Next buttons
-const startButton = document.getElementById('start')
+const startButton = document.getElementById('start');
 const questionContainer = document.getElementById('question-cont');
+
+const questionElement = document.getElementById('question');
+const answerButtonsElements = document.getElementById('answer-btns');
 
 startButton.addEventListener('click', startGame);
 
@@ -31,7 +23,7 @@ console.log('start game');
 // change the start buton to questions
  startButton.classList.add('hide');
  // Sort all questions functions randomly with a completly ramdom array thanks to mathrandom method -.5
- shuffleQuestions = questions.sort(() => Math.random() - 1)
+ shuffleQuestions = questions.sort(() => Math.random() - 1);
  // current questoy array
  currentQuestionIndex = 0;
  questionContainer.classList.remove('hide');
@@ -43,7 +35,24 @@ function nextQuestion() {
 
 }
 
+//Show question
+function showQuestion(question){
+    questionElement.innerText = question.question
+}
 
 function selectAnswer(){
 
 }
+
+// questions of the quiz
+const questions = [
+    {
+        question: 'How old is the Earth?',
+        answers: [
+            { text: '4.54 billion years old', correct: true },
+            { text: '10 billion years old', correct: false },
+            { text: '1.71 billion years old', correct: false },
+            { text: '3.96 billion years old', correct: false }
+        ]
+    }
+];
