aeec0636d83715532045fb03f07e7910fa35c7d7
Juandavidc08 <>
2024-01-04 16:31:23 +0000

    Set correct and incorrect choices working

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ console.log('start game');
 // change the start buton to questions
  startButton.classList.add('hide');
  // Sort all questions functions randomly with a completly ramdom array thanks to mathrandom method -.5
- shuffleQuestions = questions.sort(() => Math.random() - 1);
+ shuffleQuestions = questions.sort(() => Math.random() - .5);
  // current questoy array
  currentQuestionIndex = 0;
  questionContainer.classList.remove('hide');
@@ -51,7 +51,7 @@ function nextQuestion() {
 //Function to reset everything in the container back to default everytime there is a new question
 function resetState(){
     nextButton.classList.add('hide');
-    //loop all children of answer buttons
+    //loop all children for answer buttons elements
     while (answerButtonsElements.firstChild){
         answerButtonsElements.removeChild(answerButtonsElements.firstChild);
     }
@@ -61,11 +61,11 @@ function resetState(){
 function showQuestion(question){
     questionElement.innerText = question.question;
     //see answers and determine qhich one is correct
-    question.answer.forEach(answer => {
+    question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('btn');
-        if(answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
         //show the answers in the container
@@ -74,7 +74,31 @@ function showQuestion(question){
     })
 }
 
-function selectAnswer(){
+//Selected button function creates the interaction of colors dependig on answers
+function selectAnswer(e){
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct)
+    //check on everybutton to get the correct
+    Array.from(answerButtonsElements.children).forEach(button =>{
+        setStatusClass(button, button.dataset.correct)
+    })
+    //show again next button to continue the quiz
+    nextButton.classList.remove('hide');
+    }
 
+//Set status if he answer is correct or not 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('incorrect');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('incorrect');
 }
 
