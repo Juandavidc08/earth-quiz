42cc4772bcdde1593ca071048cef669ec1163dbc
Juandavidc08 <>
2024-01-04 14:10:09 +0000

    Add basic structue of quiz in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// main variables of the quiz
+
+const startButton = document.getElementById('start')
+
+startButton.addEventListener('click', startGame)
+
+// function basics structure of the quiz
+
+function startGame() {
+ console.log('start')
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
