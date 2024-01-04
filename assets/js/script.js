3862c36572ce2da29d9dbcb6ce7d4ee6a3df45bc
Juandavidc08 <>
2024-01-04 15:27:31 +0000

    Add question 1 and function to shuffle everytime

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-// main variables of the quiz
+// questions of the quiz
+const questions = [
+    {
+    question: 'How old is the Earth?'
+    answers: [
+        { text: '4.54 billion years old', correct : true },
+        { text: '10 billion years old', correct : false },
+        { text: '1.71 billion years old', correct : false },
+        {text: '3.96 billion years old', correct: false}
+    ]
+    }
+]
 
+// Shuffle question so everytime the quiz have diferent order
+const shuffleQuestions, currentQuestionIndex 
+
+//Start and Next buttons
 const startButton = document.getElementById('start')
 const questionContainer = document.getElementById('question-cont');
 
@@ -15,12 +30,19 @@ console.log('start game');
 
 // change the start buton to questions
  startButton.classList.add('hide');
+ // Sort all questions functions randomly with a completly ramdom array thanks to mathrandom method -.5
+ shuffleQuestions = questions.sort(() => Math.random() - 1)
+ // current questoy array
+ currentQuestionIndex = 0;
  questionContainer.classList.remove('hide');
+ nextQuestion();
 }
 
 function nextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestionIndex]);
 
 }
+
 
 function selectAnswer(){
 
